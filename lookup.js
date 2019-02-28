53c791cd19c3b78f36f4bdd2589eec5f50b15830
Andrew Bancroft <ABancroft@SummitHealthcareServices.local>
2019-02-28 14:11:07 -0500

    Updated formatted, add link

--- lookup.js
+++ lookup.js
@@ -10,11 +10,21 @@ app.controller("displayField", function($scope,$rootScope, demoService) {
     var t = demoService.GetData()
     $scope.ReceivedData = t;
     for (element in t) {
-      if (element != "link") {
+      if (element == "link") {
+        var url = t[element]
+        document.getElementById(element).disabled = false
+        document.getElementById(element).onclick = function() {
+          window.open(url,'_blank')
+          console.log("HELP ME")
+        }
+      } else {
         document.getElementById(element).value = t[element]
       }
     }
   });
+  function Launch() {
+    console.log("HELP")
+  }
 });
 
 //this controller is the 'input'
